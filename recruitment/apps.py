72ecab1d05120c77c72f7b3e292fde5fea172db8
milestoneconsultancy <milestoneconsultancy.in@gmail.com>
2026-08-31 03:20:14 +0530

    Fixed recruitment app config for Vercel

--- recruitment/apps.py
+++ recruitment/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
 
 class RecruitmentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recruitment'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "recruitment"
 
     def ready(self):
+        # Vercel वर URL appending टाळा
         pass
